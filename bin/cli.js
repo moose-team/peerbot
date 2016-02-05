38ae39de47f8bb81eba16b97810fbd9caa3bdba5
Feross Aboukhadijeh <feross@feross.org>
2016-02-05 14:37:24 -0800

    standard 6 prep

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-process.chdir(__dirname + '/../')
+var path = require('path')
+process.chdir(path.join(__dirname, '..'))
 
 var exec = require('shelljs').exec
 exec('electron-spawn . ' + process.argv.slice(2).join(' ') + ' 2>&1 | ./node_modules/.bin/silence-chromium')
